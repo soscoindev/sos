b813cad8529ce8c6540c3052a6b772e73b54f9d2
soscoin <esoscoin@scryptmail.com>
2016-07-04 20:05:17 +0200

    s.o.s.

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SOSCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SOSCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
